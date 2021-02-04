129578e214fd4b5d90481286dda5f2a2811473ec
Eduardo Conde <eduardo.c.pires@outlook.com>
2021-02-04 15:58:25 -0300

    -- Criação dos Método Salvar (sobrecarga de métodos);

--- Teste.Model/Servico.cs
+++ Teste.Model/Servico.cs
@@ -14,6 +14,7 @@ namespace Teste.Model {
             Funcionarios func = new Funcionarios();
             func.Id_funcionario = Convert.ToInt64(dtr["id_funcionario"]);
             func.Login_funcionario = Convert.ToString(dtr["login_funcionario"]);
+            func.Senha_funcionario = Convert.ToString(dtr["senha_funcionario"]);
             return func;
         }
         public static Clientes objCliente(ref NpgsqlDataReader dtr) {
@@ -37,6 +38,21 @@ namespace Teste.Model {
             contrato.Funcionario_contrato = objFuncionario(ref dtr);
             contrato.Data_contrato = Convert.ToDateTime(dtr["data_contrato"]);
             return contrato;
+        }
+        public static void Salvar(Funcionarios func) {
+            string sql;
+            sql = "INSERT INTO funcionarios(id_funcionario, login_funcionario, senha_funcionario) VALUES(" + func.Id_funcionario + ", '" + func.Login_funcionario + "', '" + func.Senha_funcionario + "')";
+            ConexaoBanco.executar(sql);
+        }
+        public static void Salvar(Clientes cliente) {
+            string sql;
+            sql = "INSERT INTO clientes(id_cliente, nome_cliente, cpf_cliente, arquivo_cliente, funcionario_cliente) VALUES(" + cliente.Id_cliente + ", '" + cliente.Nome_cliente + "', '" + cliente.Cpf_cliente + "', '" + cliente.Arquivo_cliente + "', " + cliente.Funcionario_cliente.Id_funcionario + ")";
+            ConexaoBanco.executar(sql);
+        }
+        public static void Salvar(Contratos contrato) {
+            string sql;
+            sql = "INSERT INTO contratos(id_contrato, num_contrato, cliente_contrato, produto_contrato, vencimento_contrato, valor_contrato, arquivo_contrato, funcionario_contrato, data_contrato) VALUES(" + contrato.Id_contrato + ", '" + contrato.Num_contrato + "', " + contrato.Cliente_contrato.Id_cliente + ", '" + contrato.Produto_contrato + "', " + contrato.Vencimento_contrato + ", " + contrato.Valor_contrato + ", '" + contrato.Arquivo_contrato + "', " + contrato.Funcionario_contrato.Id_funcionario + ", " + contrato.Data_contrato + ")";
+            ConexaoBanco.executar(sql);
         }
     }
 }
