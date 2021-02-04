b8be75307b21272b8c536bf31275c2934bcf3299
Eduardo Conde <eduardo.c.pires@outlook.com>
2021-02-04 13:32:06 -0300

    Criação da Entidade Funcionario

--- Teste.Model/Entidades.cs
+++ Teste.Model/Entidades.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Teste.Model.Entidades {
     class Entidades {
+        public class Funcionarios {
+            private Int64 id_funcionario;
+            private string login_funcionario;
+            private string senha_funcionario;
 
+            public Int64 Id_funcionario {
+                get {
+                    return id_funcionario;
+                }
+                set {
+                    id_funcionario = value;
+                }
+            }
+
+            public string Login_funcionario {
+                get {
+                    return login_funcionario;
+                }
+                set {
+                    login_funcionario = value; //fazer verificação de tamanho de nome de usuário
+                }
+            }
+
+            public string Senha_funcionario {
+                get {
+                    return senha_funcionario;
+                }
+                set {
+                    senha_funcionario = value; //fazer verificação de tamanho de senha
+                }
+            }
         }
     }
 }
