6f7b882f517fe92ef0a42bc5fe0cff21d69614d3
Eduardo Conde <eduardo.c.pires@outlook.com>
2021-02-04 19:28:40 -0300

    -- Alterações iniciais na Tela principal;

--- TesteForms/frmPrincipal.Designer.cs
+++ TesteForms/frmPrincipal.Designer.cs
@@ -26,9 +26,14 @@ namespace TesteForms {
         private void InitializeComponent() {
             this.lblTitulo = new System.Windows.Forms.Label();
             this.lblContratosLocal = new System.Windows.Forms.Label();
-            this.lstContratosLocal = new System.Windows.Forms.ListBox();
             this.grpContratosLocal = new System.Windows.Forms.GroupBox();
+            this.grpDatabase = new System.Windows.Forms.GroupBox();
+            this.lblContratosDatabase = new System.Windows.Forms.Label();
+            this.lstContratosDatabase = new System.Windows.Forms.ListBox();
+            this.lstContratosLocal = new System.Windows.Forms.ListBox();
+            this.btnSelecionarLocal = new System.Windows.Forms.Button();
             this.grpContratosLocal.SuspendLayout();
+            this.grpDatabase.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitulo
@@ -51,37 +56,81 @@ namespace TesteForms {
             this.lblContratosLocal.TabIndex = 1;
             this.lblContratosLocal.Text = "Contratos: local";
             // 
-            // lstContratosLocal
-            // 
-            this.lstContratosLocal.FormattingEnabled = true;
-            this.lstContratosLocal.Location = new System.Drawing.Point(12, 51);
-            this.lstContratosLocal.Name = "lstContratosLocal";
-            this.lstContratosLocal.Size = new System.Drawing.Size(299, 342);
-            this.lstContratosLocal.TabIndex = 2;
-            // 
             // grpContratosLocal
             // 
             this.grpContratosLocal.Controls.Add(this.lblContratosLocal);
             this.grpContratosLocal.Controls.Add(this.lstContratosLocal);
             this.grpContratosLocal.Location = new System.Drawing.Point(23, 72);
             this.grpContratosLocal.Name = "grpContratosLocal";
-            this.grpContratosLocal.Size = new System.Drawing.Size(317, 409);
+            this.grpContratosLocal.Size = new System.Drawing.Size(379, 409);
             this.grpContratosLocal.TabIndex = 3;
             this.grpContratosLocal.TabStop = false;
             this.grpContratosLocal.Text = "Arquivos .CSV";
+            // 
+            // grpDatabase
+            // 
+            this.grpDatabase.Controls.Add(this.lstContratosDatabase);
+            this.grpDatabase.Controls.Add(this.lblContratosDatabase);
+            this.grpDatabase.Location = new System.Drawing.Point(408, 72);
+            this.grpDatabase.Name = "grpDatabase";
+            this.grpDatabase.Size = new System.Drawing.Size(379, 409);
+            this.grpDatabase.TabIndex = 4;
+            this.grpDatabase.TabStop = false;
+            this.grpDatabase.Text = "PostgreSQL";
+            // 
+            // lblContratosDatabase
+            // 
+            this.lblContratosDatabase.AutoSize = true;
+            this.lblContratosDatabase.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContratosDatabase.Location = new System.Drawing.Point(6, 16);
+            this.lblContratosDatabase.Name = "lblContratosDatabase";
+            this.lblContratosDatabase.Size = new System.Drawing.Size(260, 31);
+            this.lblContratosDatabase.TabIndex = 2;
+            this.lblContratosDatabase.Text = "Contratos: database";
+            // 
+            // lstContratosDatabase
+            // 
+            this.lstContratosDatabase.FormattingEnabled = true;
+            this.lstContratosDatabase.Location = new System.Drawing.Point(12, 51);
+            this.lstContratosDatabase.Name = "lstContratosDatabase";
+            this.lstContratosDatabase.Size = new System.Drawing.Size(351, 342);
+            this.lstContratosDatabase.TabIndex = 3;
+            // 
+            // lstContratosLocal
+            // 
+            this.lstContratosLocal.FormattingEnabled = true;
+            this.lstContratosLocal.Location = new System.Drawing.Point(12, 51);
+            this.lstContratosLocal.Name = "lstContratosLocal";
+            this.lstContratosLocal.Size = new System.Drawing.Size(351, 342);
+            this.lstContratosLocal.TabIndex = 2;
+            // 
+            // btnSelecionarLocal
+            // 
+            this.btnSelecionarLocal.Location = new System.Drawing.Point(23, 488);
+            this.btnSelecionarLocal.Name = "btnSelecionarLocal";
+            this.btnSelecionarLocal.Size = new System.Drawing.Size(75, 23);
+            this.btnSelecionarLocal.TabIndex = 5;
+            this.btnSelecionarLocal.Text = "Selecionar";
+            this.btnSelecionarLocal.UseVisualStyleBackColor = true;
+            this.btnSelecionarLocal.Click += new System.EventHandler(this.btnSelecionarLocal_Click);
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(809, 526);
+            this.Controls.Add(this.btnSelecionarLocal);
+            this.Controls.Add(this.grpDatabase);
             this.Controls.Add(this.grpContratosLocal);
             this.Controls.Add(this.lblTitulo);
             this.Name = "frmPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Teste - Concilig";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
             this.grpContratosLocal.ResumeLayout(false);
             this.grpContratosLocal.PerformLayout();
+            this.grpDatabase.ResumeLayout(false);
+            this.grpDatabase.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,8 +140,12 @@ namespace TesteForms {
 
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.Label lblContratosLocal;
-        private System.Windows.Forms.ListBox lstContratosLocal;
         private System.Windows.Forms.GroupBox grpContratosLocal;
+        private System.Windows.Forms.GroupBox grpDatabase;
+        private System.Windows.Forms.Label lblContratosDatabase;
+        private System.Windows.Forms.ListBox lstContratosDatabase;
+        private System.Windows.Forms.ListBox lstContratosLocal;
+        private System.Windows.Forms.Button btnSelecionarLocal;
     }
 }
 
--- TesteForms/frmPrincipal.cs
+++ TesteForms/frmPrincipal.cs
@@ -7,11 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TesteForms {
     public partial class frmPrincipal : Form {
         public frmPrincipal() {
             InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e) {
+            StreamReader stream = new StreamReader(@"C:\tudo\teste_concilig\files\contratos.csv", Encoding.Default);
+            string linha = null;
+            while((linha = stream.ReadLine()) != null) {
+                string[] coluna = linha.Split(';');
+                string cadeia1 = PadBoth(coluna[2], 20);
+                string cadeia2 = PadBoth(coluna[4], 10);
+                string cadeia3 = PadBoth(coluna[5], 20);
+                string cadeia4 = PadBoth(coluna[3], 35);
+                string cadeia_final = cadeia1 + cadeia2 + cadeia3 + cadeia4;
+                lstContratosLocal.Items.Add(cadeia_final);
+            }
+        }
+
+        public string PadBoth(string source, int length) {
+            int spaces = length - source.Length;
+            int padLeft = spaces / 2 + source.Length;
+            return source.PadLeft(padLeft).PadRight(length);
+
+        }
+
+        private void btnSelecionarLocal_Click(object sender, EventArgs e) {
+
         }
     }
 }
