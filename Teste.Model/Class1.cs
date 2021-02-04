fc24cf25e07a19870d62b7bd410976a3d512c7e9
Eduardo Conde <eduardo.c.pires@outlook.com>
2021-02-04 12:53:24 -0300

    Criação da variável de conexão, métodos Conectar e Desconectar

--- Teste.Model/Class1.cs
+++ Teste.Model/Class1.cs
@@ -8,6 +8,30 @@ using System.Data;
 
 namespace Teste.Model.Suporte {
     public class ConexaoBanco {
+        //Criação da variável de conexão: 
+        static string stringConexao = "Server = localhost; Database = testeConcilig; User ID = postgres; password = postgres";
 
+        static NpgsqlConnection cn;
+
+        public static void conectar() {
+            if(cn == null) {
+                cn = new NpgsqlConnection();
+            }
+            try {
+                if(cn.State != ConnectionState.Open) {
+                    cn.ConnectionString = stringConexao;
+                    cn.Open();
+                }
+            }
+            catch(NpgsqlException err) {
+                throw new ApplicationException(err.Message);
+            }
+        }
+
+        public static void desconectar() {
+            cn.Close(); // fecha a conexão com o banco de dados
+            cn.Dispose(); // libera os recursos utilizados
+            cn = null;
+        }
     }
 }
